526c4ad54112f0e86cec9b8f17755cee6414d3e8
wakamenod <wakamenod@gmail.com>
2023-05-23 09:41:04 +0900

    SignInページ/Profileページの表示#19

--- mobile/lib/src/features/authentication/presentation/profile/custom_profile_screen.dart
+++ mobile/lib/src/features/authentication/presentation/profile/custom_profile_screen.dart
@@ -2,17 +2,17 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomSignInScreen extends ConsumerWidget {
-  const CustomSignInScreen({super.key});
+class CustomProfileScreen extends ConsumerWidget {
+  const CustomProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final authProviders = [EmailAuthProvider()];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign in'),
+        title: const Text('Profile'),
       ),
-      body: SignInScreen(
+      body: ProfileScreen(
         providers: authProviders,
       ),
     );
