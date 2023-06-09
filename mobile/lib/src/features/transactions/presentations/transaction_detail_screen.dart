466e26e24408d648db988600094cf1a572285427
wakamenod <wakamenod@gmail.com>
2023-06-10 08:06:05 +0900

    [Mobile] トランザクション登録 画面遷移対応(2) 旧モーダル内のウィジェットを移す #123

--- mobile/lib/src/features/transactions/presentations/transaction_detail_screen.dart
+++ mobile/lib/src/features/transactions/presentations/transaction_detail_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:suito/src/common_widgets/text_input_field.dart';
+
+import 'expense_categories_autocomplete.dart';
+import 'expense_locations_autocomplete.dart';
 
 class TransactionDetailScreen extends ConsumerWidget {
   const TransactionDetailScreen({super.key});
@@ -7,12 +11,34 @@ class TransactionDetailScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('TransactionDetail'),
-      ),
-      body: const Column(
-        children: [],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('TransactionDetail'),
+        ),
+        body: SingleChildScrollView(
+          child: ListBody(
+            children: [
+              TextInputField(
+                  errorText: "Error",
+                  hintText: "Title",
+                  onChanged: (value) => {}),
+              TextInputField(
+                  errorText: "Error",
+                  hintText: "Amount",
+                  inputType: InputType.digits,
+                  onChanged: (value) => {}),
+              TextInputField(
+                  hintText: "Date",
+                  inputType: InputType.date,
+                  onChanged: (value) => {}),
+              const ExpenseCategoriesAutocomplete(),
+              const ExpenseLocationsAutocomplete(),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Close"))
+            ],
+          ),
+        ));
   }
 }
