28357c26f47c20e6712aeb040a3afc7e33819409
wakamenod <wakamenod@gmail.com>
2023-07-19 21:53:04 +0900

    [Mobile][Test] トランザクション詳細取得テスト追加 #323

--- mobile/test/src/features/transactions/services/income/income_form_controller_test.dart
+++ mobile/test/src/features/transactions/services/income/income_form_controller_test.dart
@@ -1,14 +1,31 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:openapi/openapi.dart';
 import 'package:suito/src/features/transaction_attributes/services/transaction_attribute_entry.dart';
+import 'package:suito/src/features/transactions/repositories/income/income_detail_repository.dart';
+import 'package:suito/src/features/transactions/repositories/income/income_types_repository.dart';
 import 'package:suito/src/features/transactions/services/income/income.dart';
 import 'package:suito/src/features/transactions/services/income/income_form_controller.dart';
 
+import '../../../../mocks.dart';
+
 void main() {
+  late MockIncomeDetailRepository incomeDetailRepository;
+  late MockIncomeTypesRepository incomeTypesRepository;
+
+  setUp(() {
+    incomeDetailRepository = MockIncomeDetailRepository();
+    incomeTypesRepository = MockIncomeTypesRepository();
+  });
+
   ProviderContainer makeProviderContainer() {
     final container = ProviderContainer(
-      overrides: [],
+      overrides: [
+        incomeDetailRepositoryProvider
+            .overrideWithValue(incomeDetailRepository),
+        incomeTypesRepositoryProvider.overrideWithValue(incomeTypesRepository),
+      ],
     );
     addTearDown(container.dispose);
     return container;
@@ -109,6 +126,45 @@ void main() {
       expect(state.memo, memo);
       expect(state.date, date);
       expect(state.isValid, true);
+    });
+  });
+
+  group('incomeFuture', () {
+    test('do not fetch income if id is null', () async {
+      // setup
+      final container = makeProviderContainer();
+      // run
+      final income = await container.read(incomeFutureProvider().future);
+      // check
+      expect(income.id, '');
+    });
+
+    test('do fetch income if id is not null', () async {
+      // setup
+      final container = makeProviderContainer();
+      final incomeType = ModelIncomeType((e) => e
+        ..id = 'income_location_id'
+        ..name = 'Test Location');
+      final modelIncome = ModelIncome((e) => e
+        ..id = 'test_income_id'
+        ..incomeTypeId = incomeType.id
+        ..amount = 400
+        ..memo = ''
+        ..localDate = '2023-05-03');
+      when(() => incomeDetailRepository.fetchIncomeDetail(modelIncome.id))
+          .thenAnswer((invocation) => Future.value(
+              IncomeDetailRes((b) => b.income.replace(modelIncome))));
+      when(() => incomeTypesRepository.fetchIncomeTypesList())
+          .thenAnswer((invocation) => Future.value([incomeType]));
+      // run
+      final income =
+          await container.read(incomeFutureProvider(id: modelIncome.id).future);
+      // check
+      expect(income.id, modelIncome.id);
+      expect(income.title.value, incomeType.name);
+      expect(income.amount.value, modelIncome.amount);
+      expect(income.memo, modelIncome.memo);
+      expect(income.incomeTypeID, incomeType.id);
     });
   });
 }
